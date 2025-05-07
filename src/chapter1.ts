5552d626e717a2c869446efa5870e18dc529c8ae
yujunggg <a90134627@gmail.com>
2025-05-07 14:51:28 +0900

    feat: first commit

---
+++ src/chapter1.ts
@@ -0,0 +1,29 @@
+// number
+let num1:number = 123;
+let num2:number = -456;
+let num3:number = 0.123;
+let num4:number = -0.12345678;
+let num5:number = Infinity;
+let num6:number = NaN;
+
+// string
+let str1:string = "Hello, world!";
+let str2:string = 'Hello, world!';
+let str3:string = `Hello, world!`;
+let str4:string = `Hello, ${str1}`;
+
+// boolean
+let bool1:boolean = true;
+let bool2:boolean = false;
+
+// null
+let null1:null = null;
+
+// undefined
+let undefined1:undefined = undefined;
+
+// 리터럴
+// 리터럴 ->  값
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
---
+++ src/chapter2.ts
@@ -0,0 +1,2 @@
+// 배열
+let numArr:number[]  = [1,2,3];
