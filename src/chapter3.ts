fc70fb15422ee70c701023d88d8bbf22a2172ca1
yujunggg <a90134627@gmail.com>
2025-05-09 09:40:30 +0900

    feat : '객체'

---
+++ src/chapter3.ts
@@ -0,0 +1,18 @@
+// object
+let user: {
+    id : number;
+    name : string;
+} = {
+    id: 1,
+    name : '이유정',
+};
+
+let dog : {
+    name : string;
+    color : string;
+} = {
+    name: '멍멍이',
+    color : 'brown',
+}
+
+user.id;
