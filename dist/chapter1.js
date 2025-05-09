60d8ee291c28b24f52cd2a049d7ed8a4cf3dd437
yujunggg <a90134627@gmail.com>
2025-05-09 09:35:45 +0900

    feat: typescript tuple

---
+++ dist/chapter1.js
@@ -0,0 +1,25 @@
+// number
+let num1 = 123;
+let num2 = -456;
+let num3 = 0.123;
+let num4 = -0.12345678;
+let num5 = Infinity;
+let num6 = NaN;
+// string
+let str1 = "Hello, world!";
+let str2 = 'Hello, world!';
+let str3 = `Hello, world!`;
+let str4 = `Hello, ${str1}`;
+// boolean
+let bool1 = true;
+let bool2 = false;
+// null
+let null1 = null;
+// undefined
+let undefined1 = undefined;
+// 리터럴
+// 리터럴 ->  값
+let numA = 10;
+let strA = "hello";
+let boolA = true;
+export {};
--- src/chapter2.ts
+++ src/chapter2.ts
@@ -7,4 +7,24 @@ let boolArr:Array<boolean> = [true, false, true];
 
 
 // 배열에 들어가는 요소들의 타입이 다양할 경우
-let multiArr = [1,"hello"];
+let multiArr: (string | number)[] = [1,"hello"];
+
+// 다차원 배열의 타입을 정의하는 방법
+let doubleArr: number[][] = [
+    [1,2,3],
+    [4,5]
+]
+
+// 튜플
+// 튜플 -> 길이와 타입이 정해져 있는 배열
+let tup1: [number, number] = [1,2];
+
+let tup2: [number, string, boolean] = [1, "hello", true];
+
+const users : [string, number][]= [
+    ["winterlood", 1],
+    ["winterlood2", 2],
+    ["winterlood3", 3],
+    ["winterlood4", 4],
+    // [5, "winterlood5"]
+]
